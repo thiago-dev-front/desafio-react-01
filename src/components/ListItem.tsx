fa67b5987e759158bca40eb7ff2804fef708d9a4
Thiago <aizen.maia@gmail.com>
2022-11-13 20:48:10 -0300

    criação ListItemOff

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,20 @@
-import './ListItem.scss';
-
+import "./ListItem.scss";
+import {Trash} from 'phosphor-react'
 export function ListItem() {
-    return (
-        <h1></h1>
-    )
+  return (
+    <div className="list-topic">
+      <div className="list-topic__content">
+        <div className="list-topic__content__round">
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox"></label>
+        </div>
+        <div className="list-topic__content__text">
+            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+        </div>
+        <div className="list-topic__content__icon">
+              <Trash size={24} />
+        </div>
+      </div>
+    </div>
+  );
 }
